21de452f268d1fc773d8f907d396fa01f1c07192
vareenia <vareenia@gmail.com>
2024-05-22 09:23:38 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 resource "azurerm_resource_group" "example" {
-  name     = "RGTFPIPE"
-  location = "West Europe"
+  name     =var.rgname
+  location = var.loc
 }
 
 resource "azurerm_managed_disk" "example" {
-  name                 = "acctestmd"
+  name                 = var.diskname
   location             = azurerm_resource_group.example.location
   resource_group_name  = azurerm_resource_group.example.name
   storage_account_type = "Standard_LRS"
   create_option        = "Empty"
-  disk_size_gb         = "10"
+  disk_size_gb         = var.size
 
   tags = {
     environment = "staging"
